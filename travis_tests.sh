3be4d35ef18b5abea48c17d09d9956dbcb3cbdc5
Martijn van Maurik <git@vmaurik.nl>
2017-07-10 10:33:33 +0200

    Add softether

--- travis_tests.sh
+++ travis_tests.sh
@@ -36,6 +36,8 @@ docker build --pull --no-cache --rm --force-rm=true --tag combro2k/alpine-prosod
 docker rmi combro2k/alpine-prosody:latest
 docker build --pull --no-cache --rm --force-rm=true --tag combro2k/alpine-openfire:latest alpine-openfire
 docker rmi combro2k/alpine-openfire:latest
+docker build --pull --no-cache --rm --force-rm=true --tag combro2k/alpine-softether:latest alpine-softether
+docker rmi combro2k/alpine-softether:latest
 
 # Build dependendable containers
 docker build --pull --no-cache --rm --force-rm=true --tag combro2k/alpine-nginx-php5:latest alpine-nginx-php5

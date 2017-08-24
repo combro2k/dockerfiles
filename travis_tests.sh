487d6593f13f5016be177f0e8d4491fd6a7de1ca
Martijn van Maurik <github@vmaurik.nl>
2017-08-24 22:07:35 +0200

    Fix build errors

--- travis_tests.sh
+++ travis_tests.sh
@@ -10,36 +10,36 @@ cd ${BASE}
 docker build --pull --no-cache --rm --force-rm=true --tag combro2k/alpine-base:latest alpine-base
 
 # Standalone containers
-docker build --pull --no-cache --rm --force-rm=true --tag combro2k/alpine-posty-api:latest alpine-posty-api
+docker build --no-cache --rm --force-rm=true --tag combro2k/alpine-posty-api:latest alpine-posty-api
 docker rmi combro2k/alpine-posty-api:latest
-docker build --pull --no-cache --rm --force-rm=true --tag combro2k/alpine-posty-webui:latest alpine-posty-webui
+docker build --no-cache --rm --force-rm=true --tag combro2k/alpine-posty-webui:latest alpine-posty-webui
 docker rmi combro2k/alpine-posty-webui:latest
-docker build --pull --no-cache --rm --force-rm=true --tag combro2k/alpine-netdata:latest alpine-netdata
+docker build --no-cache --rm --force-rm=true --tag combro2k/alpine-netdata:latest alpine-netdata
 docker rmi combro2k/alpine-netdata:latest
-docker build --pull --no-cache --rm --force-rm=true --tag combro2k/alpine-haproxy:latest alpine-haproxy
+docker build --no-cache --rm --force-rm=true --tag combro2k/alpine-haproxy:latest alpine-haproxy
 docker rmi combro2k/alpine-haproxy:latest
 docker build --pull --no-cache --rm --force-rm=true --tag combro2k/alpine-teamspeak3:latest alpine-teamspeak3
 docker rmi combro2k/alpine-teamspeak3:latest
-docker build --pull --no-cache --rm --force-rm=true --tag combro2k/alpine-prosody:latest alpine-prosody
+docker build --no-cache --rm --force-rm=true --tag combro2k/alpine-prosody:latest alpine-prosody
 docker rmi combro2k/alpine-prosody:latest
-docker build --pull --no-cache --rm --force-rm=true --tag combro2k/alpine-openfire:latest alpine-openfire
+docker build --no-cache --rm --force-rm=true --tag combro2k/alpine-openfire:latest alpine-openfire
 docker rmi combro2k/alpine-openfire:latest
 docker build --pull --no-cache --rm --force-rm=true --tag combro2k/alpine-softether:latest alpine-softether
 docker rmi combro2k/alpine-softether:latest
-docker build --pull --no-cache --rm --force-rm=true --tag combro2k/alpine-aptly:latest alpine-aptly
+docker build --no-cache --rm --force-rm=true --tag combro2k/alpine-aptly:latest alpine-aptly
 docker rmi combro2k/alpine-aptly:latest
-docker build --pull --no-cache --rm --force-rm=true --tag combro2k/alpine-virtualmail:latest alpine-virtualmail
+docker build --no-cache --rm --force-rm=true --tag combro2k/alpine-virtualmail:latest alpine-virtualmail
 docker rmi combro2k/alpine-virtualmail:latest
 docker build --pull --no-cache --rm --force-rm=true --tag combro2k/alpine-djigzo:latest alpine-djigzo
 docker rmi combro2k/alpine-djigzo:latest
-docker build --pull --no-cache --rm --force-rm=true --tag combro2k/alpine-fluentd:latest alpine-fluentd
+docker build --no-cache --rm --force-rm=true --tag combro2k/alpine-fluentd:latest alpine-fluentd
 docker rmi combro2k/alpine-fluentd:latest
-docker build --pull --no-cache --rm --force-rm=true --tag combro2k/alpine-spiderfeet:latest alpine-spiderfeet
+docker build --no-cache --rm --force-rm=true --tag combro2k/alpine-spiderfeet:latest alpine-spiderfeet
 docker rmi combro2k/alpine-spiderfeet:latest
 
 # Build dependendable containers
-docker build --pull --no-cache --rm --force-rm=true --tag combro2k/alpine-nginx-php5:latest alpine-nginx-php5
-docker build --pull --no-cache --rm --force-rm=true --tag combro2k/alpine-nginx-php7:latest alpine-nginx-php7
+docker build --no-cache --rm --force-rm=true --tag combro2k/alpine-nginx-php5:latest alpine-nginx-php5
+docker build --no-cache --rm --force-rm=true --tag combro2k/alpine-nginx-php7:latest alpine-nginx-php7
 
 # Dependend PHP5 containers
 docker build --no-cache --rm --force-rm=true --tag combro2k/alpine-nginx-wordpress:latest alpine-nginx-wordpress

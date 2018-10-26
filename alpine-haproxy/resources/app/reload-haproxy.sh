da4236247b475f9b3899299ea63df9da36677664
combro2k <git@vmaurik.nl>
2018-10-26 12:13:10 +0200

    Fix haproxy

--- alpine-haproxy/resources/app/reload-haproxy.sh
+++ alpine-haproxy/resources/app/reload-haproxy.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
+if ! haproxy -c -f /etc/haproxy/haproxy.cfg -f /etc/haproxy/haproxy.cfg -f /data/haproxy.d/; then
+  echo "Not reloading haproxy because there are errors"
+
+  exit 1
+fi
 s6-svc -t /var/run/s6/services/50_haproxy/
 
 exit 0

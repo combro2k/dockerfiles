293adf8a061a37736cb590b26af3a995e492d12a
combro2k <git@vmaurik.nl>
2018-11-06 10:54:49 +0100

    Autolearn from folders via crontab

--- alpine-virtualmail-fuglu/resources/usr/local/bin/learnSA.sh
+++ alpine-virtualmail-fuglu/resources/usr/local/bin/learnSA.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env sh
 
+echo "=== SpamAssassin: Learning from SPAM ==="
 find /var/vmail/ -type d -iname '.Spam' -exec /usr/local/bin/sa-learn --no-sync --spam {} \;
-find /var/vmail/ -type d -iname '.Ham' -exec /usr/local/bin/sa-learn --no-sync --ham {} \;
+echo ""
 
+echo "=== SpamAssassin: Learning from HAM ==="
+find /var/vmail/ -type d -iname '.Ham' -exec /usr/local/bin/sa-learn --no-sync --ham {} \;
+echo ""
+
+echo "=== SpamAssassin: Sync Bayesian"
 /usr/local/bin/sa-learn --sync
+
+echo "=== SpamAssassin: Bayesian Statistics"
 /usr/local/bin/sa-learn --dump magic

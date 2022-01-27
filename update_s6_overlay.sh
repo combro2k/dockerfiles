c48131ae734ff97a689b126a32a0bc85f493f7ac
Martijn van Maurik <github@vmaurik.nl>
2022-01-27 11:12:40 +0100

    Update dockerfiles

--- update_s6_overlay.sh
+++ update_s6_overlay.sh
@@ -4,10 +4,11 @@ set -e
 #set -x
 
 BASE=$(readlink -f $(dirname $0))
-VERSION=$1
+CURVERSION=$1
+NEWVERSION=$2
 
-if [ -z ${VERSION} ]; then
-  echo "Please run with version number! Ex. ${0} v1.21.2.2"
+if [ -z ${CURVERSION} ] || [ -z ${NEWVERSION} ]; then
+  echo "Please run with version number! Ex. ${0} 1.21.2.2 1.21.2.3"
   exit 1
 fi
 
@@ -17,7 +18,7 @@ for file in */Dockerfile; do
 	DIR=$(dirname ${file})
 	IMAGE=$(basename ${DIR})
 
-	sed -i "s#\(https://github.com/just-containers/s6-overlay/releases/download\)/[^/]*/\(s6-overlay-amd64.tar.gz\)#\1/${VERSION}/\2#g" ${file}
+  sed -i "s#\(https://github.com/just-containers/s6-overlay/releases/download\)/[^/]*/\(s6-overlay-x86_64-${CURVERSION}.tar.xz\)#\1/v${NEWVERSION}/s6-overlay-x86_64-${NEWVERSION}\.tar\.xz#g" ${file}
 
 	#break
 done

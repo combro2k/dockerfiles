b8917ab7b42d7e1ef96cd9aeea33429b652317f0
Martijn van Maurik <github@vmaurik.nl>
2022-02-18 20:31:30 +0100

    Fix haproxy issues with starting container

--- alpine-haproxy/resources/app/reload-haproxy.sh
+++ alpine-haproxy/resources/app/reload-haproxy.sh
@@ -1,11 +1,17 @@
-#!/usr/bin/env sh
+#!/command/with-contenv sh
 
-if ! haproxy -c -f /etc/haproxy/haproxy.cfg -f /data/haproxy.d/; then
+while ! test -f /tmp/haproxy.pid; do
+  echo "Waiting 1 second haproxy to start up ..."
+  sleep 1
+done
+
+if ! /usr/sbin/haproxy -c -f /etc/haproxy/haproxy.cfg -f /data/haproxy.d/; then
   echo "Not reloading haproxy because there are errors"
 
   exit 1
 fi
 
+echo "Reloading haproxy"
 /command/s6-svc -t /var/run/s6/legacy-services/50_haproxy/
 
 exit 0
